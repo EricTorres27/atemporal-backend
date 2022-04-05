e9d847610405459a2c1e501314dd62b98b681c38
Daniel Cu Sánchez <danielcu@alternet.com.mx>
2022-04-04 23:53:09 -0500

    disable auth

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,11 @@ import routerPublic from './public.routes'
 import routerEvent from './events.routes'
 import routerTicket from './tickets.routes'
 import routerCategory from './categories.routes'
-import { verifyToken } from '../middlewares/auth.middleware'
+// import { verifyToken } from '../middlewares/auth.middleware'
 
 export const initRoutes = (app) => {
   app.use('/', routerPublic)
-  app.use(verifyToken)
+  // app.use(verifyToken)
   app.use('/usuarios', routerUser)
   app.use('/eventos', routerEvent)
   app.use('/boletos', routerTicket)
