f072919ed72efbc0f857c41ca8da7de0fb10a7c6
edsonvaz <e9819@outlook.com>
2022-05-15 20:01:46 -0500

    CLOUDINARY API

--- src/app.js
+++ src/app.js
@@ -17,6 +17,20 @@ app.use(morgan('dev')) // Muestra en consola la url, tiempo y status solicitado
 // Iniciar rutas
 initRoutes(app)
 
+// Image Upload
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ limit: '50mb', extended: true }))
+
+app.post('/api/upload', (req, res) => {
+  try {
+    const fileStr = req.body.data
+    console.log(fileStr)
+  } catch (error) {
+    console.error(error)
+    console.log('UHOH')
+  }
+})
+
 module.exports = {
   PORT,
   app
